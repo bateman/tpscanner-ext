cc4d4710bc23e6ef00329e0f05b83cda15a841e1
Fabio Calefato <fabio.calefato@uniba.it>
2025-03-02 12:25:10 +0100

    chore: Export extractBestPriceShippingIncluded function in scraping.js

--- js/scraping.js
+++ js/scraping.js
@@ -64,7 +64,7 @@ export function extractPricesPlusShipping(htmlContent) {
     return results;
 }
 
-function extractBestPriceShippingIncluded(htmlContent) {
+export function extractBestPriceShippingIncluded(htmlContent) {
     let item = {};
     let itemName = "";
     try {
